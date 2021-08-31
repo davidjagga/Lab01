c0702c534738760c0396f9f5f616468288817f15
davidJagga <djjagga@gmail.com>
2021-08-31 13:34:45 -0400

    lab02 commit

--- app/src/main/java/com/jaggadavid/lab01/MainActivity.java
+++ app/src/main/java/com/jaggadavid/lab01/MainActivity.java
@@ -9,46 +9,20 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button incrementButton;
-    TextView greetingDisplay;
+
+    TextView image;
     int count =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        incrementButton = findViewById(R.id.increment_button);
-        greetingDisplay = findViewById(R.id.greeting_textview);
-        incrementButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                count+=4;
-                System.out.println("incrementing: " + count);
-                Log.i("incrementing", count+"");
-                greetingDisplay.setText(count+"");
 
-            }
-        });
+        image = findViewById(R.id.image);
+
+
     }
 
-    public void decrement(View view) {
-        count -=3;
-        System.out.println("decrementing: " + count);
-        Log.i("decrementing", count+"");
-        greetingDisplay.setText(count+"");
-    }
-
-    public void restart(View view) {
-        count =0;
-        System.out.println("restart: " + count);
-        Log.i("restart", count+"");
-        greetingDisplay.setText(count+"");
-    }
-
-    public void times_2(View view) {
-        count *= 2;
-        System.out.println("Multiplying by 2: " + count);
-        Log.i("Multiplying by 2", count+"");
-        greetingDisplay.setText(count+"");
+    public void kingJulienSpeak(View view) {
     }
 }
